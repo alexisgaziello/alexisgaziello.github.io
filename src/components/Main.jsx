3827715d9446452d288ece680f1f915a8a3ff479
Alexis Gaziello <agaziello@bostonlighthouse.us>
2021-06-10 22:10:09 +0100

    reactga + history + dotenv

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 
+
 export default function Main() {
   return <Button>Click me</Button>;
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import ReactGA from 'react-ga';
 import 'semantic-ui-css/semantic.min.css';
 
-import Main from "./Main.jsx";
+import Main from "./components/Main.jsx";
+import './assets/scss/style.scss';
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+ReactGA.initialize('GOOGLE_ANALYTICS_TRACKING_ID');
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <Main />
+  </Router>,
+  document.getElementById('root')
+);
