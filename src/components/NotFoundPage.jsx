f47d567e5545054df6dd172e1c09c3a06ae4622c
Alexis Gaziello <agaziello@bostonlighthouse.us>
2021-06-14 13:34:02 +0100

    lints

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -25,23 +25,23 @@ const NotFoundPage = () => {
     <Container>
       <Segment style={{ padding: '8em 0em' }} vertical>
         {/* TODO: change color to global variable */}
-        <Header as='h1' style={{ fontSize: '15vw', fontFamily: "Billabong", color: "#85e2ff"}}  textAlign="center">
+        <Header as='h1' style={{ fontSize: '15vw', fontFamily: "Billabong", color: "#85e2ff" }} textAlign="center">
           page not found
         </Header>
-        <br/>
+        <br />
         <Header as='h2' textAlign="center">
           Classic 404 error...
         </Header>
         <Header as='h3' textAlign="center">
           Is the URL correctly formatted?
         </Header>
-        <br/>
-        <br/>
-        <br/>
+        <br />
+        <br />
+        <br />
         <Grid>
           <Grid.Column textAlign="center">
             <Button size='huge' onClick={() => { history.push("/") }} align="center">
-            Go back to home!
+              Go back to home!
             </Button>
           </Grid.Column>
         </Grid>
